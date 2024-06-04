69d9b5329cd390422fd459484d517d063e7acf08
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-06-04 16:35:55 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 //if deno
-import { Hono } from "npm:hono";
+//import { Hono } from "npm:hono";
 
 //if nodejs
 //import { Hono } from "hono";
+
+import { Hono } from "https://deno.land/x/hono/mod.ts";
+import {
+  serveStatic
+} from "https://deno.land/x/hono/middleware.ts";
 
 
 
